fc6cca4f1124d59ff521cff3745cef9d5fc2d117
huiming <hero19931012@gmail.com>
2020-12-03 09:42:47 +0800

    seeder photo

--- seeders/seederData/product.js
+++ seeders/seederData/product.js
@@ -1,12 +1,35 @@
 const productList = []
 
-for (let i = 0; i < 100; i++) {
-  let charCodeA = Math.floor(Math.random()*26) + 65
-  let charCodeB = Math.floor(Math.random()*26) + 65
-
-  let num = Math.ceil(Math.random()*1000)
-
-  console.log(String.fromCharCode(charCodeA, charCodeB) + '-' + num.toString());
+function getRandomCharCode() {
+  return Math.floor(Math.random() * 26) + 65
 }
 
+for (let i = 0; i < 20; i++) {
+  let name = String.fromCharCode(
+    getRandomCharCode(),
+    getRandomCharCode(),
+    getRandomCharCode(),
+    getRandomCharCode(),
+    getRandomCharCode(),
+  )
+  let model = String.fromCharCode(getRandomCharCode(), getRandomCharCode()) + '-' + Math.ceil(Math.random() * 1000).toString();
+  let price = Math.ceil(Math.random() * 100) * 100
+  let total = 100
+  let sell = Math.floor(Math.random() * 10) * 10
+  let storage = total - sell;
+
+  productList.push({
+    name,
+    model,
+    price,
+    storage,
+    sell,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+}
+
+// console.log(productList);
+
 module.exports = { productList };
+
