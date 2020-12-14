14c89ef5752f67355ac3ccf4f8fb68614bcb159b
huiming <hero19931012@gmail.com>
2020-12-14 19:49:33 +0800

    <feat> imgur still testing

--- controllers/image.js
+++ controllers/image.js
@@ -1,6 +1,7 @@
 const { render } = require("ejs");
 const axios = require('axios')
 const { CLIENT_ID } = require('../.env/env');
+const FormData = require('form-data');
 
 const imageController = {
   upload: (req, res) => {
@@ -8,27 +9,38 @@ const imageController = {
   },
 
   handleUpload: (req, res) => {
-    // console.log("name", req.body.name);
+    const name = req.body.name
     const image = req.file.buffer.toString("base64")
-    // console.log(image);
+    console.log("name", name);
+    console.log("image", image);
+
+    const formData = new FormData()
+    formData.append("name", name)
+    formData.append("image", image)
 
     axios({
-      method: 'POST',
-      url: 'https://api.imgur.com/3/upload',
-      data: {
-        image,
-        // tyep: "base64"
-      },
+      method: "POST",
+      url: "https://api.imgur.com/3/upload",
       headers: {
-        Authorization: `Berear ae034ab18cfcd92d1085f4a55429c228e24bb169`
+        // Authorization: `Client-ID ${CLIENT_ID}`
+        AuthorizationL: `Berear 96dab5a4ae9edfbc27aaee9fda914ce953c3bcce`
       },
-      // mimeType: 'multipart/form-data',
+      data: formData,
+
+
+      // {
+      //   image,
+      //   // name,
+      //   // album: "mzjS4yw",
+      //   type: "base64"
+      // },
+      mimeType: 'multipart/form-data',
     })
-      .then(res => { res.data })
-      .catch(err => { res.status(500).json({
-        ok: 0,
-        message: err.toString()
-      }) })
+      .then(res => { res.status(201).send(res.json) })
+      .catch((err) => {
+        console.log(err);
+        res.status(err.response.status).end()
+      })
   }
 }
 
