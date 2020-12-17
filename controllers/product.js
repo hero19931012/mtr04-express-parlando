2a25675942ded9eabc4b7d55b9a6c9607571a49a
huiming <hero19931012@gmail.com>
2020-12-17 13:38:52 +0800

    <feat> get one product by id

--- controllers/product.js
+++ controllers/product.js
@@ -28,20 +28,41 @@ const productController = {
     Product.findOne({
       where: {
         id: req.params.id
-      }
+      },
+      include: [Product_model]
     })
-      .then((product) => {
-        res.status(200).json({
-          ok: 1,
-          product
-        });
+    .then((product) => {
+      // console.log(product);
+
+      // 只回傳 id, colorChip, storage
+      const models = product.Product_models
+      .filter((model) => { return model.storage > 0 })
+      .map((model) => {
+        const { id, colorChip, storage } = model
+        return {
+          id,
+          colorChip,
+          storage: storage > 10 ? true : false
+        }
       })
-      .catch(err => {
-        res.status(400).json({
-          ok: 0,
-          errorMessage: err.toString()
-        })
-      });
+
+      res.status(200).json({
+        ok: 1,
+        models
+      })
+    })
+      // .then((product) => {
+      //   res.status(200).json({
+      //     ok: 1,
+      //     product
+      //   });
+      // })
+      // .catch(err => {
+      //   res.status(400).json({
+      //     ok: 0,
+      //     errorMessage: err.toString()
+      //   })
+      // });
   },
   add: (req, res) => {
     const product = req.body
@@ -50,35 +71,35 @@ const productController = {
       productName: product.productName,
       price: product.price
     })
-    .then(result => {
-      console.log("product create result: " + result);
-      const modelsWithProductId = product.models.map((model) => {
-        return {
-          ...model,
-          sell: 0,
-          productId: result.id
-        }
+      .then(result => {
+        console.log("product create result: " + result);
+        const modelsWithProductId = product.models.map((model) => {
+          return {
+            ...model,
+            sell: 0,
+            productId: result.id
+          }
+        })
+        return Product_model.bulkCreate(modelsWithProductId)
       })
-      return Product_model.bulkCreate(modelsWithProductId)
-    })
-    .then((result) => {
-      console.log("model create result: " + result);
-      res.status(200).json({
-        ok: 1,
-        result
+      .then((result) => {
+        console.log("model create result: " + result);
+        res.status(200).json({
+          ok: 1,
+          result
+        })
       })
-    })
-    .catch(err => {
-      let errorMessage = "product create error: " + err.toString()
-      console.log(errorMessage);
-      res.status(400).json({
-        ok: 0,
-        message: errorMessage
+      .catch(err => {
+        let errorMessage = "product create error: " + err.toString()
+        console.log(errorMessage);
+        res.status(400).json({
+          ok: 0,
+          message: errorMessage
+        })
       })
-    })
   },
   update: (req, res) => {
-  
+
   }
 }
 
