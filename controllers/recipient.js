a3aa63e1ef46be83b180a3e4eae8a8e2b0c150e4
huiming <hero19931012@gmail.com>
2020-12-24 19:07:21 +0800

    <feat> recipient getOne, add, update

--- controllers/recipient.js
+++ controllers/recipient.js
@@ -24,7 +24,14 @@ const recipientController = {
       include: [Order, Address_city, Address_district]
     })
       .then((recipient) => {
-        res.status(200).json(recipient);
+        if (req.user.role !== 'admin' && req.user.id !== recipient.Order.userId) {
+          return res.status(401).json({
+            message: "invalid user"
+          })
+        }
+        res.status(200).json({
+          recipient
+        });
       })
       .catch(err => {
         res.status(500).json({
@@ -32,9 +39,8 @@ const recipientController = {
         })
       });
   },
-  add: (req, res) => {
+  add: async (req, res) => {
     const { orderId, name, phone, email, address, cityId, districtId } = req.body;
-
     if (
       !orderId ||
       !name ||
@@ -50,21 +56,57 @@ const recipientController = {
       })
     }
 
+    // 檢查 order.userId === req.user.id
+    let order = await Order.findOne({ where: orderId })
+    if (req.user.role !== 'admin' && order.userId !== req.user.id) {
+      return res.status(401).json({
+        message: "invalid user"
+      })
+    }
+
     Recipient.create({ orderId, name, phone, email, address, cityId, districtId })
-    .then(recipient => {
-      res.status(200).json({
-        recipient
+      .then(recipient => {
+        res.status(200).json({
+          recipient
+        })
       })
-    })
-    .catch(err => {
-      console.log(`add recipient error2: ${err.toString()}`);
-      res.status(500).json({
-        message: err.toString()
+      .catch(err => {
+        console.log(`add recipient error2: ${err.toString()}`);
+        res.status(500).json({
+          message: err.toString()
+        })
       })
-    })
   },
   update: (req, res) => {
+    const { id } = req.params;
+    const { name, phone, email, address, cityId, districtId } = req.body;
+    if (
+      !name ||
+      !phone ||
+      !email ||
+      !address ||
+      !cityId ||
+      !districtId
+    ) {
+      console.log("update recipient error1: recipient data incomplete");
+      return res.status(400).json({
+        message: "recipient data incomplete"
+      })
+    }
 
+    Recipient.update(
+      { name, phone, email, address, cityId, districtId },
+      { where: { id } }
+    )
+      .then(() => {
+        res.status(204).end()
+      })
+      .catch(err => {
+        console.log(`update recipient error: ${err.toString()}`);
+        res.status(500).json({
+          message: err.toString()
+        })
+      })
   }
 }
 
