9d15a5afb1489e57ded07363ef89798ae0f904ed
huiming <hero19931012@gmail.com>
2020-12-24 12:54:34 +0800

    <fix> password 長度俢正，user 資料產生公式修正

--- migrations/20201223094507-create-admin.js
+++ migrations/20201223094507-create-admin.js
@@ -13,7 +13,7 @@ module.exports = {
         unique: true
       },
       password: {
-        type: Sequelize.STRING(20)
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
