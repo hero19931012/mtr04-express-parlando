b55a035d42f4ee93f6dd1d7f0c91a688c0a56dbb
huiming <hero19931012@gmail.com>
2020-12-31 22:28:32 +0800

    <refactor> products response format

--- controllers/v1/product.js
+++ controllers/v1/product.js
@@ -1,12 +1,13 @@
 const db = require('../../models');
 const { Product, Product_model, Photo } = db;
-
-const typeArray = ["耳罩式耳機", "入耳式耳機", "音響", "週邊配件"]
+const { productTypes } = require('../../constants/constants')
 
 const productController = {
   getAll: (req, res) => {
     let { sort, order, limit, offset, type } = req.query;
-    const option = type !== undefined ? { type, isDeleted: 0 } : { isDeleted: 0 }
+    const option = { isDeleted: 0 }
+    if (type !== undefined) { option.type = type }
+    if ((req.user === undefined || req.user !== "admin")) { option.isShow = 1 }
 
     Product.findAll({
       where: option,
@@ -24,7 +25,7 @@ const productController = {
         // 如果是 admin 就回傳全部；如果是 user，回傳 id, modelName, colorChip, storage
         if (req.user !== undefined && req.user.role === 'admin') {
           products.forEach((product) => {
-            product.type = typeArray[Number(product.type) - 1];
+            product.type = productTypes[Number(product.type) - 1];
             return
           })
 
@@ -46,7 +47,7 @@ const productController = {
               }
             })
 
-          const typeName = typeArray[Number(product.type) - 1]
+          const typeName = productTypes[Number(product.type) - 1]
           const productForUser = {
             ...product.dataValues, // 從 sequelize 物件拿出資料
             type: typeName,
@@ -71,27 +72,30 @@ const productController = {
   getOne: (req, res) => {
     const { id } = req.params
     Product.findOne({
-      where: { id, isDeleted: 0 },
+      where: { id, isDeleted: 0, isShow: 1 },
       include: [Product_model, Photo]
     })
       .then((product) => {
         // 如果找不到 => product === null
         if (product === null) {
-          console.log("get product error1: product has been deleted");
+          console.log("get product error: product has been deleted");
           return res.status(403).json({
+            success: false,
             message: "product has been deleted"
           })
         }
 
         // 設定 type
-        product.type = typeArray[Number(product.type) - 1]
+        product.type = productTypes[Number(product.type) - 1]
 
         // 如果是 admin 就回傳全部；如果是 user 只回傳 id, modelName, colorChip, storage
         if (req.user !== undefined && req.user.role === "admin") {
           return res.status(200).json({
-            product
+            success: true,
+            data: { product }
           })
         }
+
         const models = product.Product_models
           .filter((model) => { return model.storage > 0 })
           .map((model) => {
@@ -108,91 +112,82 @@ const productController = {
         productForUser.Product_models = models
 
         res.status(200).json({
-          product: productForUser
+          success: true,
+          data: { product: productForUser }
         })
       })
       .catch(err => {
+        console.log(`get one product error: ${err.toString()}`);
         res.status(500).json({
-          message: "get product error2: " + err.toString()
+          success: false,
+          message: err.toString()
         })
       });
   },
   add: (req, res) => {
     const { productName, price, type, article } = req.body;
-
     if (!productName || !price || !type || !article) {
-      console.log("add product error1: product data incomplete");
+      console.log("add product error: product data incomplete");
       return res.status(400).json({
+        success: false,
         message: "product data incomplete"
       })
-    }
-
-    let typeNum = 0
-    switch (type) {
-      case "耳罩式耳機": {
-        typeNum = 1;
-        break;
-      }
-      case "入耳式耳機": {
-        typeNum = 2;
-        break;
-      }
-      case "音響": {
-        typeNum = 3;
-        break;
-      }
-      case "週邊配件": {
-        typeNum = 4;
-        break;
-      }
-      default: { }
     }
 
     Product.create({
       productName,
       price,
-      type: typeNum,
+      type: productTypes.indexOf(type) + 1,
       article,
       isShow: 0,
       isDeleted: 0
     })
       .then((product) => {
-        res.status(200).json({ product })
+        res.status(200).json({
+          success: true,
+          data: { product }
+        })
       })
       .catch(err => {
-        console.log(`add product error2: ${err.toString()}`);
+        console.log(`add product error: ${err.toString()}`);
         res.status(500).json({
+          success: false,
           message: err.toString()
         })
       })
   },
-  update: (req, res) => {
+  update: async (req, res) => {
     const { id } = req.params
     const { productName, price, type, article, isShow, isDeleted } = req.body;
+    const product = await Product.findOne({ where: { id, isDeleted: 0 } })
 
-    Product.findOne({ where: { id, isDeleted: 0 } })
-      .then(product => {
-        if (product === null) {
-          return res.status(403).json({
-            message: "update product error1: product has been deleted"
-          })
-        }
-        return product.update({
-          productName: productName !== undefined ? productName : product.productName,
-          price: price !== undefined ? price : product.price,
-          type: type !== undefined ? type : product.type,
-          article: article !== undefined ? article : product.article,
-          isShow: isShow !== undefined ? isShow : product.isShow,
-          isDeleted: isDeleted !== undefined ? isDeleted : product.isDeleted,
-          updatedAt: new Date()
+    if (product === null) {
+      console.log("update product error: product has been deleted");
+      return res.status(403).json({
+        success: false,
+        message: "product has been deleted"
+      })
+    }
+
+    // 根據傳進什麼內容進行更新
+    product.update({
+      productName: productName !== undefined ? productName : product.productName,
+      price: price !== undefined ? price : product.price,
+      type: type !== undefined ? type : product.type,
+      article: article !== undefined ? article : product.article,
+      isShow: isShow !== undefined ? isShow : product.isShow,
+      updatedAt: new Date()
+    })
+      .then((product) => {
+        res.status(200).json({
+          success: true,
+          data: { product }
         })
       })
-      .then((product) => {
-        res.status(200).json({ product })
-      })
       .catch(err => {
-        console.log(`update product error2: ${err.toString()}`);
+        console.log(`update product error: ${err.toString()}`);
         res.status(500).json({
+          success: false,
           message: err.toString()
         })
       })
@@ -201,17 +196,20 @@ const productController = {
     const { id } = req.params
     Product.update(
       {
-         isDeleted: 1,
+        isDeleted: 1,
         updatedAt: new Date()
       },
       { where: { id } }
     )
       .then(() => {
-        res.status(204).end()
+        res.status(200).json({
+          success: true
+        })
       })
       .catch(err => {
         console.log(`delete product error: ${err.toString()}`);
         res.status(500).json({
+          success: false,
           message: err.toString()
         })
       })
