00ad0ff63d119f4a02f112ddb79f9d6a273485fe
huiming <hero19931012@gmail.com>
2020-12-31 20:43:29 +0800

    <refactor> admin / user response format

--- controllers/v1/user.js
+++ controllers/v1/user.js
@@ -1,36 +1,33 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 const db = require('../../models');
-const { SECRET, saltRounds } = process.env
+const { SECRET } = process.env
 const { User, Admin } = db;
 
 const userController = {
   handleRegister: (req, res) => {
-    const {
-      username, password, realName, email, phone
-    } = req.body;
-    if (
-      !username || !password || !realName || !email || !phone
-    ) {
-      console.log("register error1: register data incomplete");
+    const { username, password, realName, email, phone } = req.body;
+    if (!username || !password || !realName || !email || !phone) {
+      console.log("register error: register data incomplete");
       return res.status(400).json({
+        success: false,
         message: "register data incomplete"
       })
     }
 
     bcrypt.genSalt(10, (err, salt) => {
-      if (err) {
+      if (err || !salt) {
         console.log(`bcrypt genSalt error: ${err.toString()}`);
-        return res.status(500).jsno({
-          "success": false,
-          "data": null,
+        return res.status(500).json({
+          success: false,
           message: err.toString()
         })
       }
       bcrypt.hash(password, salt, (err, hash) => {
         if (err || !hash) {
-          console.log(`register error2: ${err.toString()}`);
+          console.log(`bcrypt hash error: ${err.toString()}`);
           return res.status(500).json({
+            success: false,
             message: err.toString()
           })
         }
@@ -49,37 +46,38 @@ const userController = {
               username,
               role: "user"
             }
-
-            const options = {
-              expiresIn: "7 day"
-            }
+            // expire time
+            const options = { expiresIn: "7 day" }
             jwt.sign(payload, SECRET, options, (err, token) => {
               if (err) {
-                console.log(`register error3: ${err.toString()}`);
+                console.log(`jwt sign error: ${err.toString()}`);
                 return res.status(500).json({
+                  success: false,
                   message: err.toString()
                 })
               }
               res.status(200).json({
-                token
+                success: true,
+                data: { token },
               })
             })
           })
           .catch(err => {
-            console.log(`register error4: ${err.toString()}`);
-            res.status(400).json({
-              message: "username had been registered"
+            console.log(`register error: ${err.toString()}`);
+            return res.status(400).json({
+              success: false,
+              message: "username has been registered"
             })
           });
       })
     })
-
   },
   handleLogin: (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
-      console.log(`login error1: username and password required`);
+      console.log(`login error: username and password required`);
       return res.status(400).json({
+        success: false,
         message: "username and password required"
       })
     }
@@ -87,38 +85,37 @@ const userController = {
       .then((user) => {
         bcrypt.compare(password, user.password, (err, result) => {
           if (err || !result) {
-            console.log("login error2: invalid username and password");
-            return res.status(400).json({
+            console.log(`login error: bcrypt compare false`);
+            return res.status(401).json({
+              success: false,
               message: "invalid username and password"
             })
           }
-
-          // JWT signing
           const payload = {
-            // user info
             id: user.id,
             username: user.username,
             role: "user"
           }
-          const options = {
-            expiresIn: "7 day"
-          }
+          const options = { expiresIn: "7 day" }
           jwt.sign(payload, SECRET, options, (err, token) => {
             if (err || !token) {
-              console.log(`login error2: ${err.toString()}`);
+              console.log(`jwt sign error: ${err.toString()}`);
               return res.status(500).json({
-                message: "jwt sign error"
+                success: false,
+                message: err.toString()
               })
             }
             res.status(200).json({
-              token
+              success: true,
+              data: { token },
             })
           })
         })
       })
       .catch(err => {
-        console.log(`login error3: ${err.toString()}`);
-        res.status(500).json({
+        console.log(`login error: ${err.toString()}`);
+        res.status(401).json({
+          success: false,
           message: "invalid username and password"
         })
       })
@@ -126,29 +123,34 @@ const userController = {
   getOne: async (req, res) => {
     const { id, role } = req.user
     if (role === 'admin') {
-      await Admin.findOne({ where: id })
-        .then(admin => {
-          return res.status(200).json({
+      const admin = await Admin.findOne({ where: id })
+      if (admin !== null) {
+        return res.status(200).json({
+          success: true,
+          data: {
             admin: {
               id: admin.id,
               username: admin.username
             }
-          })
+          },
         })
-      return
+      }
     }
 
     User.findOne({ where: { id } })
       .then(user => {
         const { username, realName, email, phone } = user
         res.status(200).json({
-          user: {
-            id,
-            username,
-            realName,
-            email,
-            phone
-          }
+          success: true,
+          data: {
+            user: {
+              id,
+              username,
+              realName,
+              email,
+              phone
+            }
+          },
         })
       })
   },
@@ -156,31 +158,33 @@ const userController = {
     const { id } = req.user;
     const { realName, email, phone } = req.body;
     if (!realName || !email || !phone) {
-      console.log(`update user error1: user data incomplete`);
+      console.log(`update user error: user data incomplete`);
       return res.status(400).json({
+        success: false,
         message: "user data incomplete"
       })
     }
 
-    User.findOne({ where: { id } })
-      .then((user) => {
-        return user.update({
-          realName, email, phone
-        })
-      })
+    User.update(
+      { realName, email, phone },
+      { where: { id } }
+    )
       .then((user) => {
         res.status(200).json({
-          user: {
-            id,
-            username: user.username,
-            realName,
-            email,
-            phone
-          }
+          success: true,
+          data: {
+            user: {
+              id,
+              username: user.username,
+              realName,
+              email,
+              phone
+            }
+          },
         })
       })
       .catch(err => {
-        console.log(`update user error2: ${err.toString()}`);
+        console.log(`update user error: ${err.toString()}`);
         res.status(500).json({
           message: err.toString()
         })
