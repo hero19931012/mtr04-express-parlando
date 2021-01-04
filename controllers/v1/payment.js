d88fafc443891c6ac781e265f938d653c2cb2380
huiming <hero19931012@gmail.com>
2021-01-04 17:33:30 +0800

    <refactor> recipient utils

--- controllers/v1/payment.js
+++ controllers/v1/payment.js
@@ -1,49 +1,18 @@
 const ecpay_payment = require('ecpay-payment');
-const { restart } = require('nodemon');
 const db = require('../../models');
 const { ECpay_result, Order, Product_model } = db;
-
-const getRandomMerchantTradeNo = () => {
-  let result = '';
-  for (let i = 0; i < 20; i++) {
-    if (i % 2 === 0) {
-      result += String.fromCharCode(65 + Math.floor(Math.random() * 26));
-    } else {
-      result += String.fromCharCode(48 + Math.floor(Math.random() * 10));
-    }
-  }
-  console.log("MerchantTradeNo", result);
-  return result;
-};
-
-function getDate() {
-  const date = new Date().toLocaleString()
-  let [Day, Time] = date.split(', ')
-  let [month, day, year] = Day.split('/')
-  let [time, noon] = Time.split(' ')
-  let [hr, min, sec] = time.split(':')
-  if (noon === 'PM') { hr = Number(hr) + 12 }
-
-  month = Number(month) > 10 ? month : "0" + month
-  day = Number(day) > 10 ? day : "0" + day
-  hr = Number(hr) > 10 ? hr : "0" + hr
-
-  const dateString = `${year}/${month}/${day} ${hr}:${min}:${sec}`
-  console.log(dateString);
-  return dateString
-}
+const { getDate, getRandomMerchantTradeNo } = require('../../utils')
 
 const paymentController = {
   handlePayment: async (req, res) => {
-    // 用 {uuid, MerchantTradeNo} create 一筆付款資訊, uuid 是 ecpay_result 對 order 的 foreignKey，MerchantTradeNo 在之後付款結果回傳時用來查詢要寫入的資料
-
-    const UUID = req.params.uuid;
+    const { UUID } = req.params;
 
     // 透過 uuid 取出 order
     const order = await Order.findOne({
       where: { UUID, status: 0 },
       include: [Product_model]
     });
+
     if (order === null) {
       return res.status(400).json({
         message: "invalid order id or already paid"
@@ -53,7 +22,7 @@ const paymentController = {
     // 寫入 orderId & MerchantTradeNo 進 ECpay_result
     const { id, totalPrice, Product_models } = order
 
-    if(totalPrice >= 30000) {
+    if (totalPrice >= 30000) {
       console.log("post payment error: total price must below 30,000");
       return res.status(400).json({
         message: "total price must below 30,000"
@@ -74,7 +43,7 @@ const paymentController = {
         })
       }
     } catch (err) {
-      console.log(`payment error1: ${err.toString()}`);
+      console.log(`payment error: ${err.toString()}`);
       return res.status(500).json({
         message: err.toString()
       })
