aacdf9fe636f776231c721254f40206c4ffac4b6
huiming <hero19931012@gmail.com>
2021-01-04 23:20:47 +0800

    <refactor> remove useless recipient function

--- controllers/v1/recipient.js
+++ controllers/v1/recipient.js
@@ -2,43 +2,6 @@ const db = require('../../models');
 const { Recipient, User, Order, Address_city, Address_district } = db;
 
 const recipientController = {
-  getAll: (req, res) => {
-    Recipient.findAll()
-      .then((recipient) => {
-        // if (req.user === 'admin') {
-
-        // }
-        res.status(200).json(recipient);
-      })
-      .catch(err => {
-        console.log(`get recipients error: ${err.toString()}`);
-        res.status(500).json({
-          message: err.toString()
-        })
-      });
-  },
-  getOne: (req, res) => {
-    const { id } = req.params;
-    Recipient.findOne({
-      where: { id },
-      include: [Order, Address_city, Address_district]
-    })
-      .then((recipient) => {
-        if (req.user.role !== 'admin' && req.user.id !== recipient.Order.userId) {
-          return res.status(401).json({
-            message: "invalid user"
-          })
-        }
-        res.status(200).json({
-          recipient
-        });
-      })
-      .catch(err => {
-        res.status(500).json({
-          message: "get recipient error: " + err.toString()
-        })
-      });
-  },
   add: async (req, res) => {
     const { orderId, name, phone, email, address, cityId, districtId } = req.body;
     if (
@@ -79,42 +42,6 @@ const recipientController = {
           message: err.toString()
         })
       })
-  },
-  update: (req, res) => {
-    const { id } = req.params;
-    const { name, phone, email, address, cityId, districtId } = req.body;
-    if (
-      !name ||
-      !phone ||
-      !email ||
-      !address ||
-      !cityId ||
-      !districtId
-    ) {
-      console.log("update recipient error: recipient data incomplete");
-      return res.status(400).json({
-        success: false,
-        message: "recipient data incomplete"
-      })
-    }
-
-    Recipient.update(
-      { name, phone, email, address, cityId, districtId },
-      { where: { id } }
-    )
-      .then(() => {
-        res.status(200).json({
-          success: true
-        })
-      })
-      .catch(err => {
-        console.log(`update recipient error: ${err.toString()}`);
-        res.status(500).json({
-          success: false,
-          message: err.toString()
-        })
-      })
   }
 }
-
 module.exports = recipientController;
